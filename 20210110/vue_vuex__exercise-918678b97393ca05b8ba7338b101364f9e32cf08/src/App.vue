70c2867de211802f761af5d502e36f13bbd62524
Jerry <jerry@JerrydeMacBook-Air.local>
2021-01-10 16:38:57 +0800

    5 mins for isLoading

--- 20210110/vue_vuex__exercise-918678b97393ca05b8ba7338b101364f9e32cf08/src/App.vue
+++ 20210110/vue_vuex__exercise-918678b97393ca05b8ba7338b101364f9e32cf08/src/App.vue
@@ -76,28 +76,27 @@ export default {
       cart: {
         carts: [],
       },
-      isLoading: false,
     };
   },
   methods: {
     getCart() {
       const vm = this;
-      vm.isLoading = true;
+      vm.$store.dispatch('updateLoading', true);
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
       this.$http.get(url).then((response) => {
         if (response.data.data.carts) {
           vm.cart = response.data.data;
         }
-        vm.isLoading = false;
+        vm.$store.dispatch('updateLoading', false);
         console.log('取得購物車', response.data.data);
       });
     },
     removeCart(id) {
       const vm = this;
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
-      vm.isLoading = true;
+      vm.$store.dispatch('updateLoading', true);
       this.$http.delete(url).then((response) => {
-        vm.isLoading = false;
+        vm.$store.dispatch('updateLoading', false);
         vm.getCart();
         console.log('刪除購物車項目', response);
       });
