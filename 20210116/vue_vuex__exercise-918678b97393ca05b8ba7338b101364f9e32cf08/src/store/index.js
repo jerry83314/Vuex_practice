e02fc8c6dc03124a3edc2b692d78679dec156919
Jerry <jerry@JerrydeMacBook-Air.local>
2021-01-16 17:33:57 +0800

    5 mins for cartModules

--- 20210116/vue_vuex__exercise-918678b97393ca05b8ba7338b101364f9e32cf08/src/store/index.js
+++ 20210116/vue_vuex__exercise-918678b97393ca05b8ba7338b101364f9e32cf08/src/store/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
+// import axios from 'axios';
 
-import productsModules from './product';
+import productsModules from './products';
+import cartModules from './cart';
 
 Vue.use(Vuex);
 
@@ -17,55 +18,14 @@ export default new Vuex.Store({
     updateLoading(context, status) {
       context.commit('LOADING', status);
     },
-    addtoCart(context, { id, qty }) {
-      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
-      context.dispatch('updateLoading', true);
-      const item = {
-        product_id: id,
-        qty,
-      };
-      context.dispatch('updateLoading', true);
-      axios.post(url, { data: item }).then((response) => {
-        context.dispatch('updateLoading', false);
-        console.log('加入購物車:', response);
-        context.dispatch('getCart');
-      });
-    },
-    removeCart(context, id) {
-      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
-      context.dispatch('updateLoading', true);
-      axios.delete(url).then((response) => {
-        context.dispatch('updateLoading', false);
-        context.dispatch('getCart');
-        console.log('刪除購物車項目', response);
-      });
-    },
-    getCart(context) {
-      context.dispatch('updateLoading', true);
-      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
-      axios.get(url).then((response) => {
-        if (response.data.data.carts) {
-          context.commit('CART', response.data.data);
-        }
-        context.dispatch('updateLoading', false);
-        console.log('取得購物車', response.data.data);
-      });
-    },
   },
   mutations: {
     LOADING(state, status) {
       state.isLoading = status;
     },
-    CART(state, payload) {
-      state.cart = payload;
-    },
-  },
-  getters: {
-    cart(state) {
-      return state.cart;
-    },
   },
   modules: {
     productsModules,
+    cartModules,
   },
 });
